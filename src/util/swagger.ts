2bb16e3beed3880aeefe2b0ea0aed86e934faa92
cher1shRXD <taewoo080401@gmail.com>
2024-09-18 11:13:31 +0900

    feat::swagger title

--- src/util/swagger.ts
+++ src/util/swagger.ts
@@ -8,8 +8,8 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
  */
 export function setupSwagger(app: INestApplication): void {
   const options = new DocumentBuilder()
-    .setTitle('NestJS Study API Docs')
-    .setDescription('NestJS Study API description')
+    .setTitle('Server for web study GAON')
+    .setDescription('example server for online community service')
     .setVersion('1.0.0')
     .addBearerAuth(
       {
